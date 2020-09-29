efeb2ed10f397c98519dd8a209a45615995685e4
邢墨非 <fleaglekiss@xingmofeideMacBook-Pro.local>
2020-09-29 18:45:07 +0800

    athena main part

--- code/main_server.py
+++ code/main_server.py
@@ -6,5 +6,33 @@ app = Flask(__name__)
 def dashBoard():
     return render_template('dashboard.html')
 
-if __name__ == '__main__':    
-    app.run(debug=True)    
+@app.route('/athena')
+def athena():
+    return render_template('athena.html')
+
+@app.route('/athena-service')
+def athena_service():
+    return render_template('athena-service.html')
+
+@app.route('/athena-order')
+def athena_order():
+        return render_template('athena-order.html')
+
+@app.route('/athena-statistics')
+def athena_statistics():
+        return render_template('athena-statistics.html')
+
+@app.route('/athena-balance')
+def athena_balance():
+        return render_template('athena-balance.html')
+
+@app.route('/athena-login')
+def athena_login():
+        return render_template('athena-login.html')
+
+@app.route('/athena-test')
+def athena_test():
+        return render_template('v-4.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
