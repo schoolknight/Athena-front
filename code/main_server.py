af7d0c5e85a3f34f7818575a2be448862fc9b52e
jiangzx@homedesk <jiangzx@homedesk>
2020-10-02 10:14:32 +0800

    merge xingju and add mongodb support

--- code/main_server.py
+++ code/main_server.py
@@ -1,6 +1,18 @@
 from flask import Flask
 from flask import render_template
+import configparser
+import pymongo
 app = Flask(__name__)
+
+
+# mongodb connnection
+conf= configparser.ConfigParser()
+conf.read('./server.conf')
+uri = conf.get("mongodb", "uri")
+database_str = conf.get("mongodb", "database")
+data_col_str = conf.get("mongodb", "data_col")
+mongo = pymongo.MongoClient(uri)
+data_col = mongo[database_str][data_col_str]
 
 
 patient_deal_list = [{'id':0,'t': 0}]
@@ -18,8 +30,6 @@ def aptientAgree():
     patient_deal_list.append({'id':len(patient_deal_list), 't':1})
     return 'ok'
 
-if __name__ == '__main__':    
-    app.run(debug=True)    
 @app.route('/athena')
 def athena():
     return render_template('athena.html')
